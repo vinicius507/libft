9a75bf6b82091ec60c1bfe02fc3cd8f004c03a65
Vinícius Gonçalves de Oliveira <viniciusp.olivera@gmail.com>
2021-05-29 00:27:17 -0300

    refactor: ft_lstadd_front

--- src/ft_lstadd_front.c
+++ src/ft_lstadd_front.c
@@ -14,10 +14,9 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	if (lst != NULL)
-	{
-		if (*lst != NULL)
-			new->next = *lst;
-		*lst = new;
-	}
+	if (lst == NULL)
+		return ;
+	if (*lst != NULL)
+		new->next = *lst;
+	*lst = new;
 }
