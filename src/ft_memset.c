8ea03fccca1fcddae917f9e0422f564aab0746ea
Vinícius Gonçalves de Oliveira <viniciusp.olivera@gmail.com>
2021-06-04 17:12:42 -0300

    fix: removed inline function from ft_memset

--- src/ft_memset.c
+++ src/ft_memset.c
@@ -12,39 +12,29 @@
 
 #include "libft.h"
 
-static inline void	*wordset(unsigned long int *s,
-	unsigned long int value,
-	size_t *n,
-	unsigned int bytes_per_word)
-{
-	while (*n >= bytes_per_word)
-	{
-		*s++ = value;
-		*n -= bytes_per_word;
-	}
-	return (s);
-}
-
 void	*ft_memset(void *s, int c, size_t n)
 {
+	char				*ptr;
+	unsigned long int	*lptr;
 	unsigned long int	value;
-	size_t				counter;
-	size_t				offset;
-	unsigned int		bytes_per_word;
 
-	value = (c & 0xff);
-	counter = 3;
-	while ((1 << counter) < __WORDSIZE)
-		value |= (value << (1 << counter++));
-	bytes_per_word = (1 << (counter - 3));
-	offset = 0;
-	while (((unsigned long int)s & (bytes_per_word - 1)) && n)
+	value = 0x01010101L * (c & 0xff);
+	if (sizeof(unsigned long int) > 4)
+		value |= (value << 16) << 16;
+	ptr = (char *)s;
+	while (((unsigned long int)ptr & (sizeof(unsigned long int) - 1)) && n)
 	{
-		*((char *)s + offset++) = (char)c;
+		*((char *)ptr++) = (char)c;
 		n--;
 	}
-	offset = wordset(s + offset, value, &n, bytes_per_word) - s;
+	lptr = (unsigned long int *)ptr;
+	while (n >= sizeof(unsigned long int))
+	{
+		*lptr++ = value;
+		n -= sizeof(unsigned long int);
+	}
+	ptr = (char *)lptr;
 	while (n--)
-		*((char *)s + offset++) = (char)c;
+		*((char *)ptr++) = (char)c;
 	return (s);
 }
