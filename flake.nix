06068bb32ff6c790f28ad292e2e822637e03fcf1
Vinicius Oliveira <vinicius@myriad.codes>
2023-08-17 18:27:56 -0300

    chore(nix): adds devShells.${system}.default

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    ft-nix = {
+      url = "github:vinicius507/42-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    ft-nix,
+  }: let
+    allSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
+    forEachSystem = f:
+      nixpkgs.lib.genAttrs allSystems (system:
+        f {
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ft-nix.overlays.norminette];
+          };
+        });
+  in {
+    devShells = forEachSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          bear
+          clang-tools_12
+          gnumake
+          llvmPackages_12.libcxxClang
+          norminette
+          valgrind
+        ];
+      };
+    });
+  };
+}
